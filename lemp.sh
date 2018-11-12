ad09101e2500ad995e134f8f2e4c78a295edf128
Alison Butcher <alisonkbutcher@gmail.com>
2018-11-12 17:25:07 +1000

    changes to mysql.sh

--- lemp.sh
+++ lemp.sh
@@ -48,7 +48,12 @@ cp website.com /etc/nginx/sites-available/$DOMAIN
 ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
 
 # Secure MYSQL
-mysql_secure_installation
+UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
 
 #add lets-encrypt
 add-apt-repository ppa:certbot/certbot -y
--- mysql.sh
+++ mysql.sh
@@ -1,2 +1,16 @@
-#secure mysql
-mysql_secure_installation
+#!/bin/bash
+
+#Generate / Assign Password for user
+SQLPWD=$(openssl rand -base64 6)
+# Make sure that NOBODY can access the server without a password
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$SQLPWD') WHERE User = 'root'"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
+
+echo "Mysql login details: user:root, password:$SQLPWD"
